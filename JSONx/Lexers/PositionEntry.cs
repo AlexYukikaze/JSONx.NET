a73bd5acd5d6de0d1ee085736fa2b5745941815b
AlexYukikaze <puellamagi999@gmail.com>
2016-08-04 11:18:25 +0300

    Tokenizer

--- JSONx/Lexers/PositionEntry.cs
+++ JSONx/Lexers/PositionEntry.cs
@@ -1,14 +1,28 @@
-﻿namespace JSONx.Lexers
+﻿using System;
+
+namespace JSONx.Lexers
 {
-    public class PositionEntry
+    public class PositionEntry : IEquatable<PositionEntry>
     {
-        public int Row { get; private set; }
-        public int Column { get; private set; }
+        public int Row { get; }
+        public int Column { get; }
 
         public PositionEntry(int row, int col)
         {
             Row = row;
             Column = col;
+        }
+
+        public bool Equals(PositionEntry other)
+        {
+            if (ReferenceEquals(this, other)) return true;
+            if (ReferenceEquals(null, other)) return false;
+            return Row.Equals(other.Row) && Column.Equals(other.Column);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}:{1}", Row, Column);
         }
     }
 }
--- Tests/TestLexer.cs
+++ Tests/TestLexer.cs
@@ -1,4 +1,6 @@
-﻿using JSONx.Lexers;
+﻿using System.Linq;
+using System.Xml;
+using JSONx.Lexers;
 using NUnit.Framework;
 
 namespace Tests
@@ -15,6 +17,88 @@ namespace Tests
             var c = new Token(TokenType.Number, span);
             Assert.AreEqual(a, b);
             Assert.AreNotEqual(a, c);
+        }
+
+        [Test]
+        public void TokenizerPosition()
+        {
+            var source = "";
+            var tokenizer = new Tokenizer(() => source.ToList());
+            Assert.AreEqual(tokenizer.Position, new PositionEntry(1, 1));
+            Assert.AreEqual(tokenizer.Index, 0);
+        }
+
+        [Test]
+        public void TokenizerPeek()
+        {
+            var source = "foo";
+            var tokenizer = new Tokenizer(() => source.ToList());
+            Assert.AreEqual('f', tokenizer.Peek());
+            Assert.AreEqual('o', tokenizer.Peek(1));
+            Assert.AreEqual('o', tokenizer.Peek(2));
+            Assert.AreEqual(default(char), tokenizer.Peek(3));
+            Assert.AreEqual(default(char), '\0');
+        }
+
+        [Test]
+        public void TokenizerConsume()
+        {
+            var source = "foo";
+            var tokenizer = new Tokenizer(() => source.ToList());
+            Assert.AreEqual('f', tokenizer.Current);
+            Assert.AreEqual(new PositionEntry(1, 1), tokenizer.Position);
+            Assert.AreEqual(tokenizer.Index, 0);
+            tokenizer.Consume();
+            Assert.AreEqual('o', tokenizer.Current);
+            Assert.AreEqual(new PositionEntry(1, 2), tokenizer.Position);
+            Assert.AreEqual(tokenizer.Index, 1);
+            tokenizer.Consume();
+            Assert.AreEqual('o', tokenizer.Current);
+            Assert.AreEqual(new PositionEntry(1, 3), tokenizer.Position);
+            Assert.AreEqual(tokenizer.Index, 2);
+            tokenizer.Consume();
+            Assert.AreEqual(default(char), tokenizer.Current);
+        }
+
+        [Test]
+        public void TokenizerNewLine()
+        {
+            var source = "f\nb";
+            var tokenizer = new Tokenizer(() => source.ToList());
+            Assert.AreEqual(tokenizer.Position, new PositionEntry(1, 1));
+            Assert.AreEqual(tokenizer.Index, 0);
+            tokenizer.Consume();
+            Assert.AreEqual(tokenizer.Position, new PositionEntry(1, 2));
+            Assert.AreEqual(tokenizer.Index, 1);
+            tokenizer.Consume();
+            tokenizer.NewLine();
+            Assert.AreEqual(tokenizer.Position, new PositionEntry(2, 1));
+            Assert.AreEqual(tokenizer.Index, 2);
+            tokenizer.Consume();
+            Assert.AreEqual(tokenizer.Position, new PositionEntry(2, 2));
+            Assert.AreEqual(tokenizer.Index, 3);
+        }
+
+        [Test]
+        public void TokenizerSnapshots()
+        {
+            var source = "foo";
+            var tokenizer = new Tokenizer(() => source.ToList());
+            tokenizer.TakeSnapshot();
+            tokenizer.Consume();
+            Assert.AreEqual(tokenizer.Position, new PositionEntry(1, 2));
+            Assert.AreEqual(tokenizer.Index, 1);
+            tokenizer.RollbackSnapshot();
+            Assert.AreEqual(tokenizer.Position, new PositionEntry(1, 1));
+            Assert.AreEqual(tokenizer.Index, 0);
+        }
+
+        [Test]
+        public void TokenizerEnd()
+        {
+            var source = "";
+            var tokenizer = new Tokenizer(() => source.ToList());
+            Assert.True(tokenizer.End(), "End of file expected");
         }
     }
 }
