c3433f14b84cf60702b00d503e29e5c78e522b93
AlexYukikaze <puellamagi999@gmail.com>
2016-08-04 09:51:40 +0300

    Token & TokenEquality Test

--- Tests/TestMatcher.cs
+++ Tests/TestMatcher.cs
@@ -1,10 +1,20 @@
-﻿using NUnit.Framework;
+﻿using JSONx.Lexers;
+using NUnit.Framework;
 
 namespace Tests
 {
     [TestFixture]
     public class TestMatcher
     {
-
+        [Test]
+        public void TokenEquality()
+        {
+            var span = new TokenSpan();
+            var a = new Token(TokenType.EOF, span);
+            var b = new Token(TokenType.EOF, span);
+            var c = new Token(TokenType.Number, span);
+            Assert.AreEqual(a, b);
+            Assert.AreNotEqual(a, c);
+        }
     }
 }
