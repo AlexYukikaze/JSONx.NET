21fecaa86dedfc90a3bae2956c8db60583231c20
AlexYukikaze <puellamagi999@gmail.com>
2016-08-05 04:41:11 +0300

    Singleline comment matcher

---
+++ JSONx/Lexers/SinglelineCommentMatcher.cs
@@ -0,0 +1,27 @@
+﻿namespace JSONx.Lexers
+{
+    public class SinglelineCommentMatcher : Matcher
+    {
+        protected override Token MatchToken(Tokenizer tokenizer)
+        {
+            if (tokenizer.Current != '/' && tokenizer.Peek(1) != '/')
+                return null;
+
+            var begin = tokenizer.Position;
+            tokenizer.Consume(2);
+            while (!tokenizer.End())
+            {
+                if (tokenizer.Current == '\n')
+                {
+                    tokenizer.NewLine();
+                }
+                else
+                {
+                    tokenizer.Consume();
+                }
+            }
+
+            return new Token(TokenType.SinglelineComment, new TokenSpan(begin, tokenizer.Position));
+        }
+    }
+}
